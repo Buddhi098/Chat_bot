b724a0d9d72c2e55d0b096c0bf79f78ff11eac9a
Buddhi Nadeeshan <Buddhinadeeshan97@gmail.com>
2024-05-14 09:45:33 +0530

    app.py file

---
+++ app.py
@@ -0,0 +1,103 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.embeddings import OpenAIEmbeddings , SentenceTransformerEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_openai import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css , bot_template , user_template
+from langchain.llms import HuggingFaceHub
+def get_pdf_content(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size = 1000,
+        chunk_overlap = 200,
+        length_function = len
+    )
+
+    chunks = splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embeddings = SentenceTransformerEmbeddings(model_name = "all-MiniLM-L6-v2")
+    vectore_store = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectore_store
+
+
+def get_conversational_chain(vector_store):
+    # llm = ChatOpenAI()
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    memory = ConversationBufferMemory(memory_key='chat_history' , return_messages=True)
+    conversational_chain = ConversationalRetrievalChain.from_llm(
+        llm = llm,
+        retriever = vector_store.as_retriever(),
+        memory = memory
+    )
+    return conversational_chain
+    
+    
+def handle_user_input(question):
+    response = st.session_state.conversational_chain({"question":question})
+    st.write(user_template.replace("{{MSG}}",response["question"]) , unsafe_allow_html=True)
+    st.write(bot_template.replace("{{MSG}}",response["answer"]) , unsafe_allow_html=True)
+    
+    
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat With Multiple PDF" , page_icon=":books:")
+    
+    st.write(css , unsafe_allow_html=True)
+    
+    st.header("Chat With Multiple PDF :books:")
+    st.write("Welcome to Chat With Multiple PDF")
+    question = st.text_input("Ask Any Question from Uploaded PDFs")
+    
+    if question:
+        if st.session_state.conversational_chain:
+            handle_user_input(question)
+        else:
+            st.error("Please Upload PDFs and Click on Process Button")
+            
+        
+
+
+    with st.sidebar:
+        st.subheader('Your Documents')
+        pdf_docs = st.file_uploader("Upload PDFs On Here"  , accept_multiple_files=True)
+        
+        if "conversational_chain" not in st.session_state:
+            st.session_state.conversational_chain = None
+
+        if st.button('🚀 Process'):
+            with st.spinner('Processing...'):
+                # get pdf content
+                pdf_content = get_pdf_content(pdf_docs)
+
+                # make pdf as chunks
+                text_chunks = get_text_chunks(pdf_content)
+                # st.write(text_chunks)
+
+                # get vectorstore
+                vector_store = get_vector_store(text_chunks)
+                # st.write('length: ',vector_store.index_to_docstore_id)
+                
+                # create conversational chain
+                st.session_state.conversational_chain = get_conversational_chain(vector_store)
+                # st.write(conversational_chain)
+        
+        
+
+
+if __name__ == "__main__":
+    main()
